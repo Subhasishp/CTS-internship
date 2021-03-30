393d05ca2499523997ca499d1487d71e318b99a3
Subhasishp <44719900+Subhasishp@users.noreply.github.com>
2021-03-30 14:27:41 +0530

    Add files via upload

---
+++ Conditional statement and loop/Rectangle2.java
@@ -0,0 +1,13 @@
+/*Length and breadth of a rectangle are 5 and 7 respectively.
+Program to calculate the area and perimeter of the rectangle.*/
+class Rectangle2
+{
+	public static void main(String args[])
+	{
+		int l=5,b=7,p,a;
+		p=2*(l+b);
+		a=l*b;
+		System.out.println("Perimeter of rectangle is "+p);
+		System.out.println("Area of rectangle is "+a);
+	}
+}
